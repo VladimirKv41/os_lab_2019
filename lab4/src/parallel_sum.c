2a26200198c8f36ea56f48e2738e0acffafaa46f
vladimirkv <kovalev4637@gmail.com>
2020-04-27 20:22:25 +0000

    Задание 6

--- lab4/src/parallel_sum.c
+++ lab4/src/parallel_sum.c
@@ -6,24 +6,9 @@
 
 
 #include "utils.h"
+#include "Sum.h"
 
-struct SumArgs {
-  int *array;
-  int begin;
-  int end;
-};
-
-int Sum(const struct SumArgs *args) {
-  int i,sum = 0;
-  // TODO: your code here 
-  for (i = 0; i < sizeof(args); i++){
-      if(i+1!=sizeof(args)) {
-            sum = GetMinMax(array, i*array_part, (i+1)*array_part);}
-        else {
-            sum = GetMinMax(array, i*array_part, (i+1)*array_part+(array_size%pnum));}
-  }
-  return sum;
-}
+#include <sys/time.h>
 
 void *ThreadSum(void *args) {
   struct SumArgs *sum_args = (struct SumArgs *)args;
@@ -38,11 +23,15 @@ int main(int argc, char **argv) {
    *	seed by command line arguments
    */
 
-  uint32_t threads_num = 0;
-  uint32_t array_size = 0;
-  uint32_t seed = 0;
+  uint32_t threads_num = atoi(argv[1]);
+  uint32_t array_size = atoi(argv[2]);
+  uint32_t seed = atoi(argv[3]);
   pthread_t threads[threads_num];
-
+  if (threads_num <= 0 || seed <= 0 || array_size <= 0) {
+    printf("Usage: %s threads_num \"num\" seed \"num\" array_size \"num\" \n",
+           argv[0]);
+    return 1;
+  }
   /*
    * TODO:
    * your code here
@@ -52,11 +41,19 @@ int main(int argc, char **argv) {
   int *array = malloc(sizeof(int) * array_size);
   GenerateArray(array, array_size, seed);
 
+  struct timeval start_time;
+  gettimeofday(&start_time, NULL);
 
   struct SumArgs args[threads_num];
   uint32_t i;
+  for (i = 0; i < threads_num; i++){
+      args[i].begin=i*array_size/threads_num;
+      args[i].end=(i+1)*array_size/threads_num;
+      if(i==threads_num-1) args[i].end+=array_size%threads_num;
+      args[i].array=array;
+  }
   for (i = 0; i < threads_num; i++) {
-    if (pthread_create(&threads[i], NULL, ThreadSum, (void *)&args)) {
+    if (pthread_create(&threads[i], NULL, ThreadSum, (void *)&args[i])) {
       printf("Error: pthread_create failed!\n");
       return 1;
     }
@@ -69,7 +66,13 @@ int main(int argc, char **argv) {
     total_sum += sum;
   }
 
+  struct timeval finish_time;
+  gettimeofday(&finish_time, NULL);
+
+  double elapsed_time = (finish_time.tv_sec - start_time.tv_sec) * 1000.0;
+  elapsed_time += (finish_time.tv_usec - start_time.tv_usec) / 1000.0;
   free(array);
   printf("Total: %d\n", total_sum);
+  printf("Elapsed time: %fms\n", elapsed_time);
   return 0;
 }
